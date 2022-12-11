bd6014b10c6179a20f4d3ddf6cf2d7aee273a6b3
nguyenquibinh <117495576+nguyenquibinh@users.noreply.github.com>
2022-12-12 02:54:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
-//import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set, child, get } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBdiqFc56IJ6ZqN-any0XrcdffbT7zeQr0",
-  authDomain: "kc326-4496d.firebaseapp.com",
-  databaseURL: "https://kc326-4496d-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "kc326-4496d",
-  storageBucket: "kc326-4496d.appspot.com",
-  messagingSenderId: "76477802407",
-  appId: "1:76477802407:web:16db5ff2e1b72ab7b0f882"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBdiqFc56IJ6ZqN-any0XrcdffbT7zeQr0",
+    authDomain: "kc326-4496d.firebaseapp.com",
+    databaseURL: "https://kc326-4496d-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "kc326-4496d",
+    storageBucket: "kc326-4496d.appspot.com",
+    messagingSenderId: "76477802407",
+    appId: "1:76477802407:web:99ee457429ca1d66b0f882",
+    measurementId: "G-EGV2XRGGDK"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
 send.addEventListener('click',(e) =>{ 
 Sends();
